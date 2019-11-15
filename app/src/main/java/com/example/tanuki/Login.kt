109363a0b2434fe150252eb7b96284c57be9c050
epascua <emily.r.pascua@jpl.nasa.gov>
2019-11-14 20:10:13 -0800

    TANUKI-01: Add basic layout

---
+++ app/src/main/java/com/example/tanuki/Login.kt
@@ -0,0 +1,12 @@
+package com.example.tanuki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/tanuki/MainActivity.kt
+++ app/src/main/java/com/example/tanuki/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    // The main activity will lead to the following pages:
+    // 1. Activity feed
+    // 2. Calendar
+    // 3. The "chat" - or finances
+    // These are all fragments as they will inherit the same navigation bar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
