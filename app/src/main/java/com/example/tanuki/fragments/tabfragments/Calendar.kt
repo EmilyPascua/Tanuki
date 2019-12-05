6137dcfca30ad1dc8f3e87e10b11431e15c7ecdd
treblegni <treblegni.longevity@gmail.com>
2019-12-05 04:46:11 -0800

    appbar: added drawer and appbar to app with tabs'

---
+++ app/src/main/java/com/example/tanuki/fragments/tabfragments/Calendar.kt
@@ -0,0 +1,58 @@
+package com.example.tanuki.fragments.tabfragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+
+import com.example.tanuki.R
+
+class Calendar : Fragment() {
+    private lateinit var pageViewModel: PageViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
+            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_calendar, container, false)
+        val textView: TextView = root.findViewById(R.id.calendar_text)
+        pageViewModel.text.observe(this, Observer<String> {
+            textView.text = "Calendar Fragment"
+        })
+
+        return root
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private const val ARG_SECTION_NUMBER = "section_number"
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+
+        @JvmStatic
+        fun newInstance(sectionNumber: Int): Calendar {
+            return Calendar().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_SECTION_NUMBER, sectionNumber)
+                }
+            }
+        }
+    }
+}
