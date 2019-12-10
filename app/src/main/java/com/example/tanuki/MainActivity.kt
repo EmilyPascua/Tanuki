dc677de7fb4282c31222a2fb2f4a554f5bc5f13b
treblegni <treblegni.longevity@gmail.com>
2019-12-10 12:10:36 -0800

    profile: added layout, fragment, item, and text views'

--- app/src/main/java/com/example/tanuki/MainActivity.kt
+++ app/src/main/java/com/example/tanuki/MainActivity.kt
@@ -74,7 +74,8 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home
+                R.id.nav_home,
+                R.id.nav_profile
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
